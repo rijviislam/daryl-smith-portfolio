6507e0977dce3f4640dad095d2169a79ba5630b2
rijviislam <rijviislam54@gmail.com>
2025-03-11 19:09:14 +0600

    feat: added blog section

---
+++ src/lenis.js
@@ -0,0 +1,10 @@
+ document.addEventListener("DOMContentLoaded", () => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  });
